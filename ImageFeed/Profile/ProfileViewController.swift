8ad7b004de00d92dc6195a8fb94446ebff54e8e9
Alfanikler <alfanikler@gmail.com>
2026-05-05 17:35:26 +0300

    feat(sprint_09): Добавлены экран профиля (верстка кодом) и экран просмотра картинки (верстка ib)

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -9,15 +9,126 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     
-    @IBOutlet private var userDescriptionLabel: UILabel!
-    @IBOutlet private var userIdLabel: UILabel!
-    @IBOutlet private var userNameLabel: UILabel!
+    private var userDescriptionLabel: UILabel!
+    private var userIdLabel: UILabel!
+    private var userNameLabel: UILabel!
 
-    @IBOutlet private var logoutButton: UIButton!
-    @IBOutlet private var userAvatarImageView: UIImageView!
+    private var logoutButton: UIButton!
+    private var userAvatarImageView: UIImageView!
     
-    
-    @IBAction private func didTapLogoutButton(_ sender: UIButton) {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupUI()
+        setupSubviews()
+    }
+
+    @objc
+    private func didTapLogoutButton(_ sender: UIButton) {
         print("Exit")
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .ypBlack
+    }
+    
+    private func setupSubviews() {
+        let baseFontSize: CGFloat = 13
+        let bigFontSize: CGFloat = 23
+        
+        userAvatarImageView = createUserAvatarImageView()
+
+        userNameLabel = createLabel(
+            withText: "Екатерина Новикова",
+            font: UIFont.systemFont(ofSize: bigFontSize, weight: .bold)
+        )
+        userIdLabel = createLabel(
+            withText: "@ekaterina_nov",
+            font: UIFont.systemFont(ofSize: baseFontSize, weight: .regular),
+            color: .ypGray
+        )
+        userDescriptionLabel = createLabel(
+            withText: "Hello, World!",
+            font: UIFont.systemFont(ofSize: baseFontSize, weight: .regular)
+        )
+
+        logoutButton = createLogoutButton()
+        
+        view.addSubview(userAvatarImageView)
+        view.addSubview(userDescriptionLabel)
+        view.addSubview(userIdLabel)
+        view.addSubview(userNameLabel)
+        view.addSubview(logoutButton)
+        
+        setupSubviewsConstraints()
+    }
+    
+    private func createUserAvatarImageView() -> UIImageView {
+        let image = UIImage(resource: .avatarExample)
+        let imageView = UIImageView(image: image)
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }
+    
+    private func createLogoutButton() -> UIButton {
+        let button = UIButton.systemButton(
+            with: .logoutButton,
+            target: self,
+            action: #selector(Self.didTapLogoutButton)
+        )
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.tintColor = .ypRed
+        
+        return button
+    }
+    
+    private func createLabel(
+        withText text: String,
+        font: UIFont,
+        color: UIColor = .ypWhite
+    ) -> UILabel {
+        let label = UILabel()
+        
+        label.text = text
+        label.font = font
+        label.textColor = color
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }
+    
+    private func setupSubviewsConstraints() {
+        let avatarSize: CGFloat = 70
+        let topInset: CGFloat = 32
+        let hInset: CGFloat = 24
+        let labelSpacing: CGFloat = 8
+
+        NSLayoutConstraint.activate([
+            // userAvatarImageView
+            userAvatarImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: topInset),
+            userAvatarImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: hInset),
+            userAvatarImageView.widthAnchor.constraint(equalToConstant: avatarSize),
+            userAvatarImageView.heightAnchor.constraint(equalToConstant: avatarSize),
+
+            // logoutButton
+            logoutButton.centerYAnchor.constraint(equalTo: userAvatarImageView.centerYAnchor),
+            logoutButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -hInset),
+
+            // userNameLabel
+            userNameLabel.leadingAnchor.constraint(equalTo: userAvatarImageView.leadingAnchor),
+            userNameLabel.topAnchor.constraint(equalTo: userAvatarImageView.bottomAnchor, constant: labelSpacing),
+
+            // userIdLabel
+            userIdLabel.leadingAnchor.constraint(equalTo: userAvatarImageView.leadingAnchor),
+            userIdLabel.topAnchor.constraint(equalTo: userNameLabel.bottomAnchor, constant: labelSpacing),
+
+            // userDescriptionLabel
+            userDescriptionLabel.leadingAnchor.constraint(equalTo: userAvatarImageView.leadingAnchor),
+            userDescriptionLabel.topAnchor.constraint(equalTo: userIdLabel.bottomAnchor, constant: labelSpacing)
+            
+        ])
     }
 }
