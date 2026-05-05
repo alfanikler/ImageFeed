192bba98db8256e06508b0d1382c1c709cd7faf1
niklerios <web.nikler@yandex.ru>
2026-05-06 02:06:47 +0300

    refactor(sprint_10): порефачил Constants

--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -8,15 +8,17 @@
 import Foundation
 
 enum Constants {
-    private static let env = ProcessInfo.processInfo.environment
-    
-    static let accessKey: String = getValue(fromEnv: "ACCESS_KEY")
-    static let secretKey: String = getValue(fromEnv: "SECRET_KEY")
-
+    static let accessKey = getValue(fromEnv: "ACCESS_KEY")
+    static let secretKey = getValue(fromEnv: "SECRET_KEY")
     static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
     static let accessScope = "public+read_user+write_likes"
     static let defaultBaseURLString = "https://api.unsplash.com"
-    
+}
+
+// MARK: - Helpers
+extension Constants {
+    private static let env = ProcessInfo.processInfo.environment
+
     private static func getValue(fromEnv key: String) -> String {
         guard let value = env[key] else {
             fatalError("Отсутствует переменная окружения \(key)")
